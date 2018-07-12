f15613d0b6f9ffd64aff1d5e0d28fa0cc4cb9085
jteng <akariass@gmail.com>
2018-07-12 15:06:22 -0400

    started work on courses

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import CourseManager from './containers/CourseManager';
+import CourseList from './containers/CourseList';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(
-    <h1>Hello World</h1>,
+    <CourseList />,
     document.getElementById('root')
 );
